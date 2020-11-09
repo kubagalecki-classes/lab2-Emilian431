16e10cf9b6e3b0584ef07d2f747e1132e83524ea
Emilian431 <emilian.horoszko@gmail.com>
2020-11-09 12:33:40 +0000

    zal

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,115 @@
 #include <iostream>
 
+class Wektor
+{
+public:
+    int n;
+    int get_dl() { return dlugosc; }
+    int get_poj()
+    {
+        std::cout << "Pojemnosc wektora " << pojemnosc << std::endl;
+        return pojemnosc;
+    }
+    void zmienDlugosc(int nd)
+    {
+        if (nd <= dlugosc) {
+            for (int i = nd; i < dlugosc; ++i) {
+                tablica[i] = 0;
+            }
+            dlugosc = nd;
+        }
+        else {
+            double* tablica2 = tablica;
+            tablica          = new double[nd];
+            for (int i = 0; i < dlugosc; ++i) {
+                tablica[i] = tablica2[i];
+            }
+            delete[] tablica2;
+            for (int i = dlugosc; i < nd; ++i) {
+                tablica[i] = 0;
+            }
+            dlugosc = nd;
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < dlugosc; ++i) {
+            std::cout << tablica[i] << std::endl;
+        }
+    }
+
+    void print_dl() { std::cout << "dlugosc=" << dlugosc << std::endl; }
+
+    Wektor(int a) : dlugosc{a}
+    {
+        n       = a;
+        tablica = new double[n];
+        for (int i = 0; i < n; ++i) {
+            tablica[i] = i + 11;
+            ++pojemnosc;
+        }
+    }
+
+    Wektor(const Wektor& w) : dlugosc{w.dlugosc}
+    // konstruktor kopiujacy
+    {
+        tablica = new double[dlugosc];
+        tablica = w.tablica;
+    }
+
+    Wektor(Wektor&& w) // move constructor
+    {
+        tablica   = w.tablica;
+        w.tablica = nullptr;
+    }
+
+    Wektor& operator=(Wektor&& other) // move operator
+    {
+        if (&other == this)
+            return *this;
+        delete tablica;
+        tablica       = other.tablica;
+        other.tablica = nullptr;
+        return *this;
+    }
+
+    Wektor& operator=(const Wektor& w) // operator przypisania
+    {
+        // Sprawdzenie czy nie przypisujemy obiektu do samego siebie
+        if (this != &w)
+            *tablica = *w.tablica;
+        return *this;
+    }
+
+    ~Wektor()
+    {
+        delete[] tablica;
+        --pojemnosc;
+    }
+    double& operator[](const int i)
+    {
+        if (i < dlugosc) {
+            double& el_vector = tablica[i];
+            return el_vector;
+        }
+        else {
+            zmienDlugosc(i);
+            double& el_vector = tablica[i];
+            return el_vector;
+        }
+    }
+    int        dlugosc;
+    static int pojemnosc;
+    double*    tablica;
+};
+
+int Wektor::pojemnosc = 0;
+
 int main()
 {
-    puts("Hello, World!");
+    Wektor v{4};
+    Wektor v2{6};
+    v2 = std::move(v);
+    v2.print();
 }
--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,53 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+
+    ResourceManager()   //konstruktor domyslny
+    {
+      d = new Resource;
+    }
+
+    ~ResourceManager()  //destruktor
+    {
+      delete d;
+    }
+
+    ResourceManager(const ResourceManager& d)
+    // konstruktor kopiujacy
+    {
+        d = new Resource;
+        Resource = d.Resource;
+    }
+
+    ResourceManager(ResourceManager&& d) // move constructor
+    {
+        Resource   = d.Resource;
+        d.Resource = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) // move operator
+    {
+        if (&other == this)
+            return *this;
+        delete Resource;
+        Resource       = other.Resource;
+        other.Resource = nullptr;
+        return *this;
+    }
+
+    ResourceManager& operator=(const ResourceManager& d) // operator przypisania
+    {
+        if (this != &d)
+            *Resource = *d.Resource;
+        return *this;
+    }
+
+     double get()
+    {
+        return d;
+    }
+
+    private:
+    Resource *d;
 };
